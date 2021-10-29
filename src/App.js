704c9cde99e90a62c39e0ff57a82af550564c609
MIB-Reece <mauriceonyeani@gmail.com>
2021-10-28 21:04:11 -0700

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useEffect } from "react"
+import { Helmet } from "react-helmet"
 import { 
   Grid, CssBaseline, Box,
   BottomNavigation, BottomNavigationAction
-} from '@material-ui/core';
-import { styled } from '@mui/material/styles';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import TwitterIcon from '@material-ui/icons/Twitter';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import YouTubeIcon from '@material-ui/icons/YouTube';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+} from '@material-ui/core'
+import { styled } from '@mui/material/styles'
+import FacebookIcon from '@material-ui/icons/Facebook'
+import TwitterIcon from '@material-ui/icons/Twitter'
+import InstagramIcon from '@material-ui/icons/Instagram'
+import YouTubeIcon from '@material-ui/icons/YouTube'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
   const ContentDiv = styled(Box)`
     flex-direction: column;
@@ -46,11 +49,20 @@ import Button from '@mui/material/Button';
   `
 
 export default function App(){
+  useEffect(() => {
+    AOS.init({})
+  }, [])
+
   return(
     <div overflow="clip">
+      <Helmet>
+        <title>Jump Button Studio</title>
+      </Helmet>
     <CssBaseline/>
     <Grid container display="flex" justifyContent="center" width="100vw" height="100vh">
-    <HeaderImg >
+    <HeaderImg data-aos="fade-down"
+            data-aos-delay="50"
+            data-aos-duration="500">
       <img
           src="https://media.discordapp.net/attachments/707955771316633720/896612185755774986/unknown.png?width=1217&height=595"
           alt="Jump Button Studio Founders"
@@ -59,10 +71,17 @@ export default function App(){
     
     <ContentDiv item justify-content="center" align-items="center">
       <div className="pageText">
-        <h4
+        <h4 data-aos="fade-up"
+            data-aos-delay="50"
+            data-aos-duration="1000"
         >increasing representation within the games and animation industry</h4>
-        <h5 
-        >we're currently working on our website and super excited to show you <br/>what we've been working on</h5>
+        <h5 data-aos="fade-up"
+            data-aos-delay="200"
+            data-aos-duration="1000"
+        >we're currently working on our website and super excited to show you what we've been working on<br/>
+          We want to reimagine what it means to be diverse and inclusive within the games and animation industry.<br/>
+          We do this by placing our storytellers and developers first <br/>
+          because we believe a diverse and inclusive studio can tell the most relatable and powerful stories.</h5>
       </div>
       <Grid container justifyContent="center" alignItems="center">
         <Grid item xs={6} sm={3} gap={{xs: .5}} noWrap className="emailInput" >
